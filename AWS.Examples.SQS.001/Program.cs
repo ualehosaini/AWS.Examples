c15a7d274f08f0a993444dc2de5240b1a651c064
UA <useme1980.2024@outlook.com>
2024-11-22 08:54:10 +0000

    Initial commit

---
+++ AWS.Examples.SQS.001/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AWS.Examples.SQS._001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
